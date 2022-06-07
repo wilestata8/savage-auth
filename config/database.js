f55d9e090d38a735fe83822dd4b0ada92d32f7e2
Aminah Davis-Wiles <102968032+wilestata8@users.noreply.github.com>
2022-06-07 15:45:03 -0400

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+module.exports = {
+
+   'url' : 'mongodb+srv://wilestata8:wilestata8@cluster1.a7x7t.mongodb.net/test?retryWrites=true&w=majority',
+    'dbName' :'test'
+
+   
+}
