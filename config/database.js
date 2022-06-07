67245bb58f807b4d7e79c16438f24474e5905f65
Aminah Davis-Wiles <102968032+wilestata8@users.noreply.github.com>
2022-06-07 15:44:13 -0400

    Delete database.js

--- config/database.js
+++
@@ -1,8 +0,0 @@
-// config/database.js
-module.exports = {
-
-   'url' : 'mongodb+srv://wilestata8:wilestata8@cluster1.a7x7t.mongodb.net/user?retryWrites=true&w=majority',
-    'dbName' :'loginInfo'
-
-   
-}
